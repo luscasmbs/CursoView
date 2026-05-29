dc3948d11922408d8dc46b6f88fd2f5650676813
luscasmbs <lucasmiguelbsilva@gmail.com>
2026-05-29 10:20:30 -0300

    feat: estrutura e inicio do header

---
+++ app/src/main/java/com/example/projetoteste/ui/components/CursoViewHeader.kt
@@ -0,0 +1,75 @@
+package com.example.projetoteste.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.projetoteste.R
+import com.example.projetoteste.ui.theme.ProjetoTesteTheme
+
+@Composable
+fun CursoViewHeader(
+    modifier: Modifier = Modifier
+){
+    Column(modifier = modifier.fillMaxWidth()){
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(170.dp)
+                .background(Color(0xFF1d3557))
+
+        ){
+            Row(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .padding(top = 46.dp),
+                verticalAlignment = androidx.compose.ui.Alignment.CenterVertically // Alinha verticalmente ao centro entre si
+            ){
+                Image(
+                    painter = painterResource(id = R.drawable.chapeu),
+                    contentDescription = "Logo do app",
+                    modifier = Modifier.size(40.dp)
+                        .clip(RoundedCornerShape(6.dp))
+                )
+                Text(
+
+                    text = "CURSOVIEW",
+                    modifier = Modifier.padding(start = 12.dp),
+                    style = MaterialTheme.typography.labelSmall,
+                    color = Color(0xFFa1baca),
+                    fontSize = 25.sp,
+
+                )
+            }
+        }
+    }
+}
+@Preview
+@Composable
+private fun HeaderPreview(){
+    ProjetoTesteTheme {
+        CursoViewHeader()
+    }
+}
---
+++ app/src/main/java/com/example/projetoteste/ui/screens/CadastroCursos.kt
@@ -0,0 +1,10 @@
+package com.example.projetoteste.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.projetoteste.ui.components.CursoViewHeader
+
+@Composable
+fun CadastroCursos(modifier: Modifier = Modifier){
+    CursoViewHeader()
+}
